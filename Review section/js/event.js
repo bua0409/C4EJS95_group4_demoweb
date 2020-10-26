d02153585b2ce0a1b2c3555d05fa60daa49f955a
Son Dinh <dnson20022001@gmail.com>
2020-10-26 16:37:31 +0700

    small update

--- Review section/js/event.js
+++ Review section/js/event.js
@@ -1,14 +1,6 @@
 //-----------------------------------------
-//Get elements and add event listener here
+//Add event listeners stay here
 //-----------------------------------------
-
-const reviewDisplay = document.getElementById("review-display"),
-  reviewInput = document.getElementById("review-input"),
-  submitBtn = document.getElementById("submit-btn"),
-  reviewBtn = document.getElementById("review-btn"),
-  reviewContainer = document.getElementById("review-container"),
-  warning = document.getElementById("warning"),
-  backBtn = document.getElementById("back-btn");
 
 submitBtn.addEventListener("click", () => {
   pushData(getUserReview());
---
+++ Review section/js/getElement.js
@@ -0,0 +1,10 @@
+//--------------------------------------
+//Get element stay here
+//--------------------------------------
+const reviewDisplay = document.getElementById("review-display"),
+  reviewInput = document.getElementById("review-input"),
+  submitBtn = document.getElementById("submit-btn"),
+  reviewBtn = document.getElementById("review-btn"),
+  reviewContainer = document.getElementById("review-container"),
+  warning = document.getElementById("warning"),
+  backBtn = document.getElementById("back-btn");
