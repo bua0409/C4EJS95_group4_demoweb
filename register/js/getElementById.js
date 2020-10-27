f5572a13b4ee0c89cdbba7c297296aa1472125e4
ZzLuffyzZ <70495382+ZzLuffyzZ@users.noreply.github.com>
2020-10-27 13:06:25 +0000

    Update getElementById.js

--- register/js/getElementById.js
+++ register/js/getElementById.js
@@ -2,6 +2,5 @@ const emailInput = document.getElementById("re");
 const passwordInput = document.getElementById("rp");
 const passwordRetypeInput = document.getElementById("rrp");
 const usernameInput = document.getElementById("un");
-const addressInput = document.getElementById("address");
 const phoneInput = document.getElementById("phone");
 const submitReg= document.getElementById("submit-reg");
