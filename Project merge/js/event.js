4297096da576060d044b0dc55bc08f19bb1049d3
Sondinh <dnson20022001@gmail.com>
2020-10-28 15:21:31 +0700

    fix login validation bug

--- Project merge/js/event.js
+++ Project merge/js/event.js
@@ -1,39 +1,43 @@
 loginButtonElement.addEventListener("click", () => {
   const username = usernameElement.value;
   const password = passwordElement.value;
+  console.log(username);
+  console.log(password);
   //validate
   for (const user of listUser) {
+    console.log(user.user);
+    console.log(user.pass);
     if (user.user === username && user.pass === password) {
       if (user.role === "hotel") {
         addClass(loginPageElement, "d-none");
-        removeClass(); // hotel page
+        // removeClass(); // hotel page
+        return;
       } else if (user.role === "user") {
         addClass(loginPageElement, "d-none");
-        removeClass(); //user page
+        // removeClass(); //user page
+        return;
       }
-    } else {
-      alert ("Invalid username and/or password")
-      break;
     }
+    // alert("Invalid username and/or password");
+    // break;
   }
+  alert("Invalid username and/or password");
 });
 
-//check naming  
-
-
+//check naming
 
 signupButtonElement.addEventListener("click", () => {
   redirectSignInToSignupScreen();
 });
 
-signupButtonSubmitElement.addEventListener("click", async() => {
+signupButtonSubmitElement.addEventListener("click", async () => {
   const resUsername = resUsernameElement.value;
   const resEmail = resEmailElement.value;
   const resPhone = resPhoneElement.value;
   const resPassword = resPasswordElement.value;
   const resRePassword = resRePasswordElement.value; /// need validate
   //validate
-  //after validate success 
+  //after validate success
 
   /*const newUser = {
     id: listUser.length + 1,
@@ -48,26 +52,23 @@ signupButtonSubmitElement.addEventListener("click", async() => {
   listUser.push(newUser);
 
   redirectSignupToSignInScreen();
-});*/
-
+;*/
 
   let reg = await notOK();
-  if(reg){
+  if (reg) {
     const newUser = {
       id: listUser.length + 1,
       email: resEmail,
       user: resUsername,
       pass: resPassword,
-      address: undefined,
       phone: resPhone,
       role: "user",
     };
-  
+
     listUser.push(newUser);
-    alert("Thanks for registration. \nTry to login Now")
-  
+    console.log(listUser);
+    alert("Thanks for registration. \nTry to login Now");
+
     redirectSignupToSignInScreen();
-  ;
-      }
   }
-)
+});
